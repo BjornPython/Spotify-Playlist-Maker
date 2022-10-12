664e9fdaba23b17d61ebab4bc0d256b820064574
BjornPython <110831700+BjornPython@users.noreply.github.com>
2022-10-12 14:31:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ USER_ID = os.environ.get("SPOTIFY_USER_ID")
 test = PlaylistMaker(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, USER_ID)
 
 # Date is a string, and is in the format yyyy-mm-dd example: "1980-10-12"
-date = ""
-print(test.make_playlist("date"))
+date = "2010-01-15"
+print(test.make_playlist(date, "2010 songs", "top 100 songs of 2010"))
